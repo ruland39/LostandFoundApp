ea5b3dbb5fde280a57a0547e91d47cd1fb4625ad
ruland39 <hcyrf1@nottingham.edu.my>
2023-11-06 12:21:34 +0800

    initialize themes and feature page

--- app/src/main/java/com/lostandfoundapp/FeatureActivity.java
+++ app/src/main/java/com/lostandfoundapp/FeatureActivity.java
@@ -27,27 +27,5 @@ public class FeatureActivity extends AppCompatActivity {
 
         binding = ActivityFeatureBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_feature);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_feature);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
